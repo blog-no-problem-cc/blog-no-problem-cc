6786103d867637bb6e277578e786ca646d1e3f7f
Blog Master <blog@no-problem.cc>
2021-06-07 00:48:49 +0200

    Initial commit

---
+++ ak/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":" ","categories": ["en"],
+        "tags": [],
+        "url": "https://blog.no-problem.cc/ak/en/hello/",
+        "teaser": null
+      },{
+        "title": "Добро пожаловать",
+        "excerpt":" ","categories": ["ru"],
+        "tags": [],
+        "url": "https://blog.no-problem.cc/ak/ru/hello/",
+        "teaser": null
+      }]
