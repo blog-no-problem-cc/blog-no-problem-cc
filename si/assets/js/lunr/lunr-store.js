92357dae603b96481c3db4fb9f84b554234d076f
Blog Master <blog@no-problem.cc>
2021-06-18 00:20:01 +0200

    ec541f6 Initial commit, refactored clone of blog-ak

---
+++ si/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":" ","categories": ["en"],
+        "tags": [],
+        "url": "https://blog.no-problem.cc/si/en/hello/",
+        "teaser": null
+      },{
+        "title": "Добро пожаловать",
+        "excerpt":" ","categories": ["ru"],
+        "tags": [],
+        "url": "https://blog.no-problem.cc/si/ru/hello/",
+        "teaser": null
+      }]
