54903c34cd01dcebbea53cef0b2fbf0a395ba969
Blog Master <blog@no-problem.cc>
2021-06-22 23:50:11 +0200

    4947e6d New (old) post

--- si/assets/js/lunr/lunr-store.js
+++ si/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Welcome",
-        "excerpt":" ","categories": ["en"],
+        "title": "Interview: HR projects with Svetlana Ilina",
+        "excerpt":"Thea Brockmeyer (CEO of RICONS Consulting GmbH, the founder of Project Leader Academy) has interviewed me and we discussed the following topics: Selection of a project management training Typical HR-projects Importance of risks analysis Behaviour and culture change projects Typical roadblocks for HR projects Tips to lead a project with...","categories": ["en"],
         "tags": [],
-        "url": "https://blog.no-problem.cc/si/en/hello/",
-        "teaser": null
-      },{
-        "title": "Добро пожаловать",
-        "excerpt":" ","categories": ["ru"],
-        "tags": [],
-        "url": "https://blog.no-problem.cc/si/ru/hello/",
+        "url": "https://blog.no-problem.cc/si/en/Interview/",
         "teaser": null
       }]
