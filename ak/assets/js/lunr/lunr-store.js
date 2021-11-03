4185801c248b9d1e00199e51c488d9107a616bbd
Blog Master <blog@no-problem.cc>
2021-11-04 00:13:01 +0100

    516e611 Enable tags

--- ak/assets/js/lunr/lunr-store.js
+++ ak/assets/js/lunr/lunr-store.js
@@ -1,31 +1,31 @@
 var store = [{
         "title": "FOSDEM 2020",
         "excerpt":"On the first weekend of February 2020, I visited Brussels to experience an exciting atmosphere of a global gathering of free software developers: FOSDEM 2020. Besides a unique opportunity to taste local beer and Belgian waffles FOSDEM provides two days filled in with conversations with interesting people, a possibility to...","categories": ["en"],
-        "tags": [],
+        "tags": ["event","fpga","riscv","lua"],
         "url": "https://blog.no-problem.cc/ak/en/fosdem-20/",
         "teaser": null
       },{
         "title": "FOSDEM 2020",
         "excerpt":"Первые выходные февраля 2020 года я провёл в Брюсселе, где погрузился в потрясающую атмосферу FOSDEM 2020 — сборища разработчиков свободного программного обеспечения со всего мира. Помимо возможности попробовать местное пиво и брюссельские вафли, FOSDEM — это два дня общения и встреч с интересными людьми, возможность узнать, что происходит в мире...","categories": ["ru"],
-        "tags": [],
+        "tags": ["event","fpga","riscv","lua"],
         "url": "https://blog.no-problem.cc/ak/ru/fosdem-20/",
         "teaser": null
       },{
         "title": "Mi-V Virtual Summit Conference 2021",
         "excerpt":"I am looking forward to make a presentation on behalf of ARIES Embedded at the first Mi-V Virtual Summit Conference. The presentation will start at 9:30 a.m. Pacific (18:30 CEST) on July, 21st, and will be followed by a live Q&amp;A session. During the conference you will be able to...","categories": ["en"],
-        "tags": [],
+        "tags": ["event","riscv","polarfire","fpga"],
         "url": "https://blog.no-problem.cc/ak/en/mi-v-announcement/",
         "teaser": null
       },{
         "title": "Онлайн конференция и выставка Mi-V Summit 2021",
         "excerpt":"В июле этого года я буду представлять ARIES Embedded на онлайн конференции, организованной Microchip для компаний-участников партнёрской программы Mi-V. Выступление будет транслироваться 21 июля в 19:30 MSK, после выступления запланировано время для ответов на вопросы. На выставке-конференции у посетителей будет возможность познакомиться со многими компаниями, участвующими в партнёрской программе Mi-V,...","categories": ["ru"],
-        "tags": [],
+        "tags": ["event","riscv","polarfire","fpga"],
         "url": "https://blog.no-problem.cc/ak/ru/mi-v-announcement/",
         "teaser": null
       },{
         "title": "Arrow to Go Webinar: Building Applications on multi-core RISC-V based on Microchip PolarFire SoC FPGA",
         "excerpt":"Preparations are on the way to co-present on the first live-session of the two-webinar series: “Building Applications on multi-core RISC-V based on Microchip Technology Inc. PolarFire SoC-FPGA”. The first session will take place on October, 13, from 9:00 to 10:30 CEST. The webinar focuses on hardware related aspects of the...","categories": ["en"],
-        "tags": [],
+        "tags": ["event","riscv","polarfire","fpga"],
         "url": "https://blog.no-problem.cc/ak/en/atg-webinar/",
         "teaser": null
       }]
