216393eb4a34f201ebb677ee70c50469b9bedfc8
Blog Master <blog@no-problem.cc>
2022-02-06 22:52:53 +0100

    d1a11de Add tags to old posts

--- si/assets/js/lunr/lunr-store.js
+++ si/assets/js/lunr/lunr-store.js
@@ -7,19 +7,19 @@ var store = [{
       },{
         "title": "Project Management: Is it a proliferation of buzz words or a vital necessity?",
         "excerpt":"Within the last decades, the importance of project management has been skyrocketing, or at least such an impression can be built. Combinations of words ‘an innovative project’, ‘a strategic project’, ‘a special project’, and countless many “projects” alike, can be met everywhere: on a website of any company, in promotional...","categories": ["en"],
-        "tags": [],
+        "tags": ["upskilling"],
         "url": "https://blog.no-problem.cc/si/en/PMandTasks/",
         "teaser": null
       },{
         "title": "PM & HR: Strangers or Buddies",
         "excerpt":"HR is often perceived by employees as a conservative and purely supportive or administrative function within a company. Even in organizations, where HR has a strong governing role and acts as a business partner on the top level, on the bottom level these HR roles might be invisible. Why it...","categories": ["en"],
-        "tags": [],
+        "tags": ["hr-project","hr-transformation","digitalisation","upskilling"],
         "url": "https://blog.no-problem.cc/si/en/PM-HR_strangers-or-buddies/",
         "teaser": null
       },{
         "title": "Hindrances in HR projects",
         "excerpt":"Usually, an HR project is started as a response to changes in business strategy, as a value-add HR initiative, digitalization of processes, or any combination of these. HR projects are diverse by purpose, nature, duration, available resources, and budget, however, some common critical issues and roadblocks can be present in...","categories": ["en"],
-        "tags": [],
+        "tags": ["stakeholder","hr-project","processes","capabilities","culture","competency","communication","risk","success-factor"],
         "url": "https://blog.no-problem.cc/si/en/Hindrances-in-HRprojects/",
         "teaser": null
       },{
@@ -31,13 +31,13 @@ var store = [{
       },{
         "title": "Chart Your Development Trail",
         "excerpt":"Today’s speed of changes and a life-tempo open for many people new development horizons. At the same time, it requires continuous learning and upskilling. If a decade ago an employee with a learning aptitude could fully rely on an employee development strategy of his/her company to keep an appropriate level...","categories": ["en"],
-        "tags": [],
+        "tags": ["learning","development","upskilling","training","certification"],
         "url": "https://blog.no-problem.cc/si/en/chart-your-developemnt-trail/",
         "teaser": null
       },{
         "title": "Critical Issues and Success Factors in HR projects: Stakeholders",
         "excerpt":"“No matter what you stand for, no matter what your ultimate purpose may be, you must take into account the effects of your actions on others, as well as their potential effects on you.” R. Edward Freeman You have received a new challenging task — to lead one of the...","categories": ["en"],
-        "tags": ["stakeholder","hr-project","risk","success-factor"],
+        "tags": ["stakeholder","hr-project","risk","success-factor","communication","commitment","analysis"],
         "url": "https://blog.no-problem.cc/si/en/Stakeholders/",
         "teaser": null
       }]
